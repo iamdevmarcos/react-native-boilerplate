3fb55bb054bcb2951fdbdde4b9b03ce73251cb64
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-23 17:56:02 -0300

    Remove exhaustive-deps rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,7 +4,6 @@ module.exports = {
   rules: {
     '@typescript-eslint/no-non-null-assertion': 'off',
     'react-hooks/rules-of-hooks': 'error',
-    'react-hooks/exhaustive-deps': 'warn',
     'react/prop-types': 'off',
     'react/react-in-jsx-scope': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off'
