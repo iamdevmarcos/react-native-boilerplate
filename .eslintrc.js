eb33976fcfe8316b273871d6593fd25abfba403d
Marcos Mendes <marcosdev.me@gmail.com>
2024-01-23 08:52:48 -0300

    Add Prettier & Eslint Config

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  root: true,
+  extends: ['universe/native'],
+  rules: {
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+    'react/prop-types': 'off',
+    'react/react-in-jsx-scope': 'off',
+    '@typescript-eslint/explicit-module-boundary-types': 'off'
+  }
+}
